8dd6e12914bde3edeb8950dbbd748d7274c9a651
rebe2167 <55078380+rebe2167@users.noreply.github.com>
2020-01-07 18:28:05 +0100

    næsten færdig

--- scrpit.js
+++ scrpit.js
@@ -1,10 +1,12 @@
-window.addEventListener("load", navShow);
+window.addEventListener("load", sidenVises);
 
+//burger menu//
 
-function navShow() {
-    console.log("navShow");
+function sidenVises() {
+    console.log("sidenVises");
 
     document.querySelector(".burger_menu").addEventListener("click", burgerClick);
+    document.querySelector(".forside_logo").addEventListener("mouseover", mouseOverForside);
 }
 
 
@@ -12,4 +14,22 @@ function burgerClick() {
     console.log("burgerClick");
 
     document.querySelector(".nav_links").classList.toggle("nav_active");
+}
+
+//burger menu slut//
+
+function mouseOverForside() {
+    console.log("mouseOverForside");
+
+    document.querySelector(".splash_pic").classList.add("mouse_zoom");
+    document.querySelector(".splash_pic").addEventListener("mouseout", mouseOutForside);
+}
+
+function mouseOutForside() {
+    console.log("mouseOutForside");
+
+    document.querySelector(".forside_logo").removeEventListener("mouseout", mouseOutForside);
+
+    document.querySelector(".splash_pic").classList.remove("mouse_zoom");
+
 }
